cfccf47a294735d66dafba5a1c84d98f967d45e0
Artur Gasparyan <gasparyanartur99@gmail.com>
2025-03-06 18:58:06 +0100

    finish data visualization

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 mkdir data/ repos/
-git clone https://github.com/mariel-pettee/choreo-graph.git repos
+git clone https://github.com/mariel-pettee/choreo-graph.git repos/choreo-graph
 cp -a repos/choreo-graph/data/* data/
